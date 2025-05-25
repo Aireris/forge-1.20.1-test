23b0ed33655c53e745b75139f7459ab60fb06b5e
kadir <kadirthicc@gmail.com>
2025-05-25 13:17:25 +0300

    added ruby

--- src/main/java/net/kadirthicc/basitmod/item/ModdedItem.java
+++ src/main/java/net/kadirthicc/basitmod/item/ModdedItem.java
@@ -2,6 +2,7 @@ package net.kadirthicc.basitmod.item;
 
 import net.kadirthicc.basitmod.basitmod;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
